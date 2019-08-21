d87fe83608bd3c4b829386dae5dd79b3311fdc2c
Master-Th <47017607+Master-Th@users.noreply.github.com>
2019-08-21 15:44:01 +0000

    Manual editing

--- UI.py
+++ UI.py
@@ -1,22 +1,25 @@
 from flask import Flask
 import socket,threading
 
-app = Flask(__name__)
-
 class Main:
 	def __init__(self):
 		[threading.Thread(target = i).start() for i in [self.Interop, self.UI]]
 
 	def Interop(self):
-		print('1')
-#                Interop = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#                Interop.connect(("localhost",4000))
-#                Interop.send(b'Take us to your leader...')
-		pass
+		print('Connecting to a server on localhost...')
+		Interop = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		while(True):
+			try:
+				Interop.connect(("localhost",4000))
+				break
+			except:pass
+		Interop.send(b'Take us to your leader...')
+		print("Was able to connect to a server on localhost, port 4000.")
 
 	def UI(self):
 		print('2')
+		app = Flask(__name__)
 		@app.route("/test")
-		def test(): return '0'
-
+		def test(): return open("./Pages/Main.html",'r').read()
+		app.run()
 Main()
