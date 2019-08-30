05ae74c7dc5b6fbed3273882d43da7785519fc7f
root <root@localhost.localdomain>
2019-08-30 18:18:29 +0000

    Added websocket functionality

--- UI.py
+++ UI.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import socket,threading
+import socket,threading,websockets
 
 class Main:
 	def __init__(self):
@@ -22,4 +22,22 @@ class Main:
 		@app.route("/test")
 		def test(): return open("./Pages/Main.html",'r').read()
 		app.run()
+
+	async def Reflect(self):
+		pass
+
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print(f"< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f"> {greeting}")
+
+start_server = websockets.serve(hello, "localhost", 6800)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+
 Main()
