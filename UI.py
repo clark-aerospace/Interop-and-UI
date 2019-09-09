c4f2a4bf8868e0380c1febed993ed71a6f65dae6
root <root@localhost.localdomain>
2019-09-09 09:28:15 +0000

    See for yourself

--- UI.py
+++ UI.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import socket,threading,websockets
+import socket,threading,websocket,random
 
 class Main:
 	def __init__(self):
@@ -21,23 +21,31 @@ class Main:
 		app = Flask(__name__)
 		@app.route("/test")
 		def test(): return open("./Pages/Main.html",'r').read()
+		@app.route("/Motivation.mp3")
+		def Motivation(): return open("./Pages/Motivation.mp3",'rb').read()
 		app.run()
 
-	async def Reflect(self):
-		pass
+	def Reflect(self):
+		def on_message(ws, message): print(message)
+		def on_error(ws, error): print(error)
+		def on_close(ws): print("[CLOSING TIME]")
+		def on_open(ws):
+			def run(*args):
+				while(True):
+					time.sleep(5)
+					ws.send(str(random.randint(0,1000000)))
+				ws.close()
+			threading.Thread(run,())
+		print("???")
+		websocket.enableTrace(True)
+		ws = websocket.WebSocketApp("127.0.0.1:6800",
+				on_message = on_message,
+				on_error = on_error,
+				on_close = on_close)
+		ws.on_open = on_open
+		ws.run_forever()
 
-async def hello(websocket, path):
-    name = await websocket.recv()
-    print(f"< {name}")
-
-    greeting = f"Hello {name}!"
-
-    await websocket.send(greeting)
-    print(f"> {greeting}")
-
-start_server = websockets.serve(hello, "localhost", 6800)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
 
 Main()
+
+if __name__ == "__main__": pass
